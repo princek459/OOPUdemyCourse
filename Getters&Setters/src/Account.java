62df0324d4eb4c8366bcfc7ee9fdc3726a56ef75
Prince kunzwa <princekunzwa@outlook.com>
2021-11-30 01:48:35 +0000

    Getters and setters initial and project final push.

---
+++ Getters&Setters/src/Account.java
@@ -0,0 +1,15 @@
+
+
+public class Account {
+
+    private int balance;
+
+//   public Setter allows anyone to set the balance
+    public void setBalance(int balance){
+        this.balance = balance;
+    }
+//   Getter returns the value of balance
+    public int getBalance() {
+        return balance;
+    }
+}
